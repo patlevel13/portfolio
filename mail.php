cd6710673b1dc4bbb506f322f00e93bb648e7041
patlevel13 <patleviok@gmail.com>
2023-12-05 16:51:52 +0100

    php et media

---
+++ mail.php
@@ -0,0 +1,24 @@
+<?php
+if($_POST) {
+    $firstname=$_POST['firstname'];
+    $email =$_POST['email'];
+    $name = $_POST['name'];
+    $message = $_POST['message'];
+
+    $headers = "MIME-Version: 1.0\r\n";
+    $headers ="Content-type: text/plain; charset=iso-8859-1\r\n";
+    $headers ="Form: $name<$email>\r\nReply-to : $name<$email>\nX-Mailer:PHP";
+/* modif email*/
+    $subject="$object";
+    $destinataire="coucou@cmoi.com";
+    $body="$message";
+if(mail($destinataire,$subject,$body,$headers)) {
+    $response['satus'] ='success';
+    $response['msg'] = 'your mail is sent';
+    } else {
+        $response['satus'] ='error';
+        $response['msg'] = 'Something went wrong';
+    }
+    echo json_encode($response)
+}
+?>
--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,40 @@ $('#open-modal').click(function () {
         });
     });
 });
+
+/*send mail with ajax*/
+$('#send-email').click(function(e){
+    e.preventDefault();
+    var data = {
+        email: $('#email').val(),
+        name: $('#name').val(),
+        firstname: $('#firstname').val(),
+        message: $('#message').val()
+    };
+    // Ajax
+    $.ajax({
+        url: "mail.php",
+        type: 'POST',
+        data: data,
+        success: function(data) {
+            $('#js_alert_success').css({'display' : 'block'});
+            setTimeout(function(){
+                $('#js_alert_success').css({'display' : 'none'});
+                $('#email').val("");
+                $('#name').val("");
+                $('#firstname').val("");
+                $('#message').val("")
+            }, 3000);
+        },
+        error: function(data) {
+            $('#js_alert_danger').css({'display' : 'block'});
+            setTimeout(function(){
+                $('#js_alert_danger').css({'display' : 'none'});
+                $('#email').val("");
+                $('#name').val("");
+                $('#firstname').val("");
+                $('#message').val("")
+            }, 3000);
+        }
+    });
+});
